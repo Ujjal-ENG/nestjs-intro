b167da3fee9c10630d92fc6ee8124a6dde2a7a65
ujjal <ujjalroy7862@gmail.com>
2025-12-30 22:23:30 +0600

    controller added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  public getUsers() {
+    return 'Users get method';
+  }
+
+  @Post()
+  public createUser() {
+    return 'This method only for create users!!';
+  }
+
+  @Patch('/:id')
+  public getUserById(@Param() params: { id: string }) {
+    return `This method for patricular user like: ${params?.id}`;
+  }
+}
