a14019dbf4b6912d75a2b8ce31fb896a53335f60
ujjal <ujjalroy7862@gmail.com>
2025-12-31 21:02:51 +0600

    globally use validation pipe in nest js

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,7 +8,6 @@ import {
   Patch,
   Post,
   Query,
-  ValidationPipe,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 
@@ -25,7 +24,7 @@ export class UsersController {
   }
 
   @Post()
-  public createUser(@Body(new ValidationPipe()) createUserDto: CreateUserDto) {
+  public createUser(@Body() createUserDto: CreateUserDto) {
     console.log(createUserDto);
     return 'This method only for create users!!';
   }
