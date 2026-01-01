a1331fdb7a29fd9b9873ac2bf6c34b9a6118b002
ujjal <ujjalroy7862@gmail.com>
2026-01-01 20:54:29 +0600

    setup the dependecy injectable in user module to post module

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../../users/providers/users.service';
 import { CreatePostDto } from '../dto/create-post.dto';
 import { UpdatePostDto } from '../dto/update-post.dto';
 
 @Injectable()
 export class PostsService {
+  // dependency injection called
+  constructor(private readonly userService: UsersService) {}
+
   create(createPostDto: CreatePostDto) {
     return 'This action adds a new post';
   }
